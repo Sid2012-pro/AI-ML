b8f76eb2d62c9aaf6318d22b2d97b9908103b503
Sid2012-pro <sidsassy28@gmail.com>
2024-06-26 17:30:09 +0530

    Update pie.py

--- pie.py
+++ pie.py
@@ -1,1 +1,25 @@
+import matplotlib.pyplot as plt
+import pandas as pd
 
+
+min_percentage = 0.035
+
+data = pd.read_csv('fifa_eda_stats.csv')
+
+
+nationalities_count = data['Nationality'].value_counts()
+
+
+total_players = len(data)
+
+
+filtered_nationalities_count = nationalities_count[nationalities_count >= min_percentage * total_players]
+
+
+other_count = total_players - filtered_nationalities_count.sum()
+if other_count > 0:
+  filtered_nationalities_count['Rest'] = other_count
+
+plt.pie(filtered_nationalities_count, labels=filtered_nationalities_count.index, autopct='%1.1f%%', radius=3)
+
+plt.show()
